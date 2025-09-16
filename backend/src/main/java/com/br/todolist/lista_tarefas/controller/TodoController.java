f804278c70188ff4c3243ff4507befffe2488a7a
Giovani <giovanifeitosa@gmail.com>
2025-09-16 10:03:12 -0300

    directories change

---
+++ backend/src/main/java/com/br/todolist/lista_tarefas/controller/TodoController.java
@@ -0,0 +1,49 @@
+package com.br.todolist.lista_tarefas.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.todolist.lista_tarefas.entity.Todo;
+import com.br.todolist.lista_tarefas.service.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+    
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+
+        return todoService.create(todo);
+    }
+    
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+        
+    }
+
+    @PutMapping
+     List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+        
+    }
+
+    @DeleteMapping("{id}")
+     List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+        
+    }
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      Olá, react!
+    </div>
+  )
+}
+
+export default App
